928ebf76eba3cea4afff004a9f701f2aadb3e269
USER001 F <USER001 F>
2016-09-16 23:29:08 +0900

    f7Q/KoreBuild CI Test
    Update KoreBuild

--- build.ps1
+++ build.ps1
@@ -33,7 +33,7 @@ cd $PSScriptRoot
 $repoFolder = $PSScriptRoot
 $env:REPO_FOLDER = $repoFolder
 
-$koreBuildZip="https://github.com/aspnet/KoreBuild/archive/1.0.0.zip"
+$koreBuildZip="https://github.com/f7q/KoreBuild/releases/download/1.0.1-bata/KoreBuild-1.0.1.zip"
 if ($env:KOREBUILD_ZIP)
 {
     $koreBuildZip=$env:KOREBUILD_ZIP
