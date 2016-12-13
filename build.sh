67b75f60cfef559d61f51bd0d5242a900af4c9ce
USER001 F <USER001 F>
2016-12-14 00:45:59 +0900

    Change Latest
    KoreBuild確認とプロジェクトのバージョンアップ

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
 
-koreBuildZip="https://github.com/f7q/KoreBuild/archive/1.0.1-bata2.zip"
+koreBuildZip="https://github.com/aspnet/KoreBuild/archive/dev.zip"
 if [ ! -z $KOREBUILD_ZIP ]; then
     koreBuildZip=$KOREBUILD_ZIP
 fi
